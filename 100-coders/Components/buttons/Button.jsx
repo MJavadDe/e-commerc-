948d6654e8aa1712a03e0e1f62cb44c5fa8d86f7
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-02 17:23:12 -0700

    1.2

---
+++ 100-coders/Components/buttons/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div>
+            <h1>button will be here</h1>
+        </div>
+    );
+};
+
+export default Button;
---
+++ 100-coders/Components/inputs/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div>
+            <h1>input will be here</h1>
+        </div>
+    );
+};
+
+export default Input;
