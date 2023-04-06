db1d7df0c71b5631b91b6cef182ba3d80a1a4a25
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-06 19:20:24 +0430

    header and footer position fixed

--- Components/navbar/Navbar.jsx
+++ Components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import "public/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="bg-[#F3FCF8] top-[30px] absolute grid grid-flow-col justify-between px-4 w-[90.92%] rounded-md">
+    <div className="bg-[#F3FCF8] grid grid-flow-col justify-between px-4 w-[90.92%] rounded-md">
       <div className="grid grid-flow-col gap-3 items-center">
         <Link href="/courses">
           <Button children={<img src="logo.png"></img>} />
