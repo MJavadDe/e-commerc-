b0d5cbe54348e55084f9b52acadab912f3fc944b
Hadis Yazdi <69729120+hadis-yzd@users.noreply.github.com>
2023-04-06 22:22:47 +0430

    services card
    its almost done but let me know if u know how to fix the pics. tnx

---
+++ Components/servicesCard/ServicesCard.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import React from 'react';
+
+import Gift from './gift.svg';
+import GraduationCap from './graduation-cap.svg';
+import SupportSormeyi from './support sormeyi.svg';
+import Teach from './teach.svg';
+
+
+function ServicesCard(props) {
+    return (
+        <div className='flex justify-center items-center'>
+           
+           <div className='m-4 flex flex-col justify-center items-center'>
+            <Image src={GraduationCap} alt="" className='bg-primary-100 rounded-full p-2 w-[50%] ' />
+            <p>دوره های رایگان</p>
+           </div>
+
+           <div className='m-4 flex flex-col justify-center items-center'>
+            <Image src={Teach} alt="" className='bg-primary-100 rounded-full p-2 w-[50%] ' />
+            <p>مدرسین مجرب</p>
+           </div>
+
+           <div className='m-4 flex flex-col justify-center items-center'>
+            <Image src={Gift} alt="" className='bg-primary-100 rounded-full p-2 w-[50%] ' />
+            <p>کدهای تخفیف</p>
+           </div> 
+
+           <div className='m-4 flex flex-col justify-center items-center'>
+            <Image src={SupportSormeyi} alt="" className='bg-primary-100 rounded-full p-2 w-[50%] '/>
+            <p>پشتیبانی 24/7</p>
+           </div>
+           
+        </div>
+    );
+}
+
+export default ServicesCard;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
 import Layout from '@/Components/layout';
 import Navbar from '@/Components/navbar/Navbar';
-import SuccessOrder from '@/Components/SuccessOrder/SuccessOrder';
 import React from 'react';
 
 const Home = () => {
   return (
     <Layout title="Home page">
+      
     </Layout>
   );
 };
