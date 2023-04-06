0ef4a156811f3aa7c3351bd629ecc838228bcec1
Hoom90 <mohammad.mahdavi0015@gmail.com>
2023-04-06 14:03:33 +0330

    message "Pay_Detail_Card"

--- Components/navbar/Navbar.jsx
+++ Components/navbar/Navbar.jsx
@@ -1,47 +1,33 @@
-import React from 'react';
-import { Button } from 'antd';
-import Link from 'next/link';
+import React from "react";
+import { Button } from "antd";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div>
-            <Link href="/courses">
-                <Button>
-                courses  
-                </Button> 
-            </Link>
-            <Link href="/blog">
-                <Button>
-                blog  
-                </Button> 
-            </Link>
-            <Link href="/connect">
-                <Button>
-                connect us
-                </Button> 
-            </Link>
-            <Link href="/aboutUs">
-                <Button>
-                about us  
-                </Button> 
-            </Link>
-            <Link href="/shopcart">
-                <Button>
-                shop cart
-                </Button>
-            </Link>
-            <Link href="/auth">
-                <Button>
-                login
-                </Button>
-            </Link>
-            <Link href="/dashbord">
-                <Button>
-                    dashbord
-                </Button>
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <Link href="/courses">
+        <Button>courses</Button>
+      </Link>
+      <Link href="/blog">
+        <Button>blog</Button>
+      </Link>
+      <Link href="/connect">
+        <Button>connect us</Button>
+      </Link>
+      <Link href="/aboutUs">
+        <Button>about us</Button>
+      </Link>
+      <Link href="/shopcart">
+        <Button>shop cart</Button>
+      </Link>
+      <Link href="/auth">
+        <Button>login</Button>
+      </Link>
+      <Link href="/dashbord">
+        <Button>dashbord</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 
   ],
   theme: {
+    colors:{
+      
+    },
     extend: {},
   },
   plugins: [],
