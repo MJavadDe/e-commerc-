20347fe769f815023da3838af00ecef9d452b391
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-06 19:42:49 +0430

    footer basis fixed

--- Components/footer&components/components/FooterBottom.jsx
+++ Components/footer&components/components/FooterBottom.jsx
@@ -5,7 +5,7 @@ const FooterBottom = () => {
     return (
         <div className='border-t-2 border-[#B1AFAF] flex items-center justify-around py-5 text-[#B4CFC3]'>
             <h1>
-                 برند به عنوان یک نام و علامت تجاری ثبت شده است. کلیه حقوق این وب سایت برای  محفوظ است 
+                   برند به عنوان یک نام و علامت تجاری ثبت شده است. کلیه حقوق این وب سایت برای  محفوظ است ©  
             </h1>
             <div className="socialMedias flex justify-between gap-x-3">
                 <span>
--- Components/footer&components/components/NamadComponent.jsx
+++ Components/footer&components/components/NamadComponent.jsx
@@ -3,12 +3,12 @@ import SpecialHead from './SpecialHead';
 import style from './Footer.module.css'
 const NamadComponent = (props) => {
     return (
-        <div>
-            <SpecialHead className={`top-0 text-[#61CE70] text-[22px] w-fit py-1`} title={props.content.title}/>
+        <div className=' basis-6/12'>
+            <SpecialHead className={`top-0 text-[#61CE70] text-[22px] w-fit py-1 `} title={props.content.title}/>
             <div className={`flex gap-7 pt-8 style ${style.bbordert}`}>
                 <div><img src={props.content.src1} className='bg-white rounded-md p-1' alt="" /></div>
                 <div><img src={props.content.src2} className='bg-white rounded-md p-1' alt="" /></div>
-                <div><img src={props.content.src3} className='bg-white rounded-md p-1' alt="" /></div>
+                <div><img src={props.content.src3} className='bg-white rounded-md p-2' alt="" /></div>
             </div>
         </div>
     );
