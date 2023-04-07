71092fe40f5604bd0e0797dee1b03c987d062fc1
Hadis Yazdi <69729120+hadis-yzd@users.noreply.github.com>
2023-04-07 17:38:47 +0430

    pagination component final version

--- Components/pagination/Pagination.jsx
+++ Components/pagination/Pagination.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
-import { Pagination } from 'antd';
-import styles from './style.css'
+
+
+import { Pagination } from "antd";
+import { useState } from "react";
+import React from "react";
+import styles from "./pagaination.module.css";
 
 function Paginations(props) {
-    const [current, setCurrent] = useState(1);
-
-  const onChange= (page) => {
+  const [current, setCurrent] = useState(3);
+  const onChange = (page) => {
+    console.log(page);
     setCurrent(page);
   };
-    return (
-          <Pagination current={current} onChange={onChange} total={80} />  
-    );
+  return (
+    <div className="flex justify-center">
+      <Pagination
+      className={styles.anticon}
+      current={current}
+      onChange={onChange}
+      total={50}
+    />
+    </div>
+  );
 }
 
 export default Paginations;
