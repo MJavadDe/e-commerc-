ca8a4422ab7db110c8526e2a42ba193201cd04b0
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-07 17:53:40 +0430

    footer responsive edit

--- Components/footer&components/components/NamadComponent.jsx
+++ Components/footer&components/components/NamadComponent.jsx
@@ -3,9 +3,9 @@ import SpecialHead from './SpecialHead';
 import style from './Footer.module.css'
 const NamadComponent = (props) => {
     return (
-        <div className=' basis-6/12'>
-            <SpecialHead className={`top-0 text-[#61CE70] text-[22px] w-fit py-1 `} title={props.content.title}/>
-            <div className={`flex gap-7 pt-8 style justify-around w-72 ${style.bbordert}`}>
+        <div className=' basis-6/12 w-1/3'>
+            <SpecialHead className={`top-0 text-[#61CE70] text-[22px] py-1 `} title={props.content.title}/>
+            <div className={`flex gap-7 pt-8 style justify-around w-full relative ${style.bbordert}`}>
                 <div><img src={props.content.src1} className='bg-secondary-100 rounded-md p-1' alt="" /></div>
                 <div><img src={props.content.src2} className='bg-secondary-100 rounded-md p-1' alt="" /></div>
                 <div><img src={props.content.src3} className='bg-secondary-100 rounded-md p-1' alt="" /></div>
