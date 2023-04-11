2e7c0361ed80a26275cddd79876e0d7268bedee9
Hoom90 <mohammad.mahdavi0015@gmail.com>
2023-04-11 14:14:17 +0330

    add-paydetail-cardlist

--- Components/card/Card.jsx
+++ Components/card/Card.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-export default function Card(prop) {
-  const item = prop.item;
+export default function Card({ name, price, currency }) {
   return (
     <div className="bg-gray-50 grid grid-flow-col grid-cols-6 gap-1 p-2 justify-center rounded items-center">
-      <div className="truncate col-span-4">{item.name}</div>
-      <div className="col-span-1">{item.price}</div>
-      <div className="col-span-1">{item.currency}</div>
+      <div className="truncate col-span-4">{name}</div>
+      <div className="col-span-1">{price}</div>
+      <div className="col-span-1">{currency}</div>
     </div>
   );
 }
---
+++ Components/payDetail cardList/CardList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "../card/Card";
+import Button from "@/Commons/buttons/Button";
+
+export default function CardList() {
+  return (
+    <div className="w-[415px] rounded-md">
+      <Card
+        name={"دوره نکست جی اس مقدماتی از صفر تا صد "}
+        price={"90.000"}
+        currency={"تومان"}
+      ></Card>
+
+      <hr></hr>
+      <Card
+        name={"دوره نکست جی اس مقدماتی از صفر تا صد "}
+        price={"90.000"}
+        currency={"تومان"}
+      ></Card>
+      <Card
+        name={"دوره نکست جی اس مقدماتی از صفر تا صد "}
+        price={"90.000"}
+        currency={"تومان"}
+      ></Card>
+      <Card
+        name={"دوره نکست جی اس مقدماتی از صفر تا صد "}
+        price={"90.000"}
+        currency={"تومان"}
+      ></Card>
+      <Card
+        name={"دوره نکست جی اس مقدماتی از صفر تا صد "}
+        price={"90.000"}
+        currency={"تومان"}
+      ></Card>
+      <hr></hr>
+      <Card
+        name={"دوره نکست جی اس مقدماتی از صفر تا صد "}
+        price={"90.000"}
+        currency={"تومان"}
+      ></Card>
+      <Button>تکمیل فرایند خرید</Button>
+    </div>
+  );
+}
