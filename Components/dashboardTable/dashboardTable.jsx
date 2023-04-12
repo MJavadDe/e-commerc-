70901d47a36937f8f7dc908fc6876d64ebccaee0
amin <94073967+aminyaghmaee@users.noreply.github.com>
2023-04-12 21:53:34 +0430

    checkoutCart added , tailwind gray category added

--- Components/dashboardTable/dashboardTable.jsx
+++ Components/dashboardTable/dashboardTable.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import PriceFormat from "@/utilitie/priceFormat";
 
-const Table = () => {
+const DashboardTable = () => {
   const courses = [
     {
       name: "دوره ریکت",
@@ -49,11 +49,11 @@ const Table = () => {
     <div dir="rtl" className="font-vazir">
       <div className="flex justify-around text-center mb-5 bg-[#46A094] text-white font-bold h-[63px] items-center">
         <h3 className="w-[10%]">تصویر</h3>
-        <h3 className="w-[10%]">نام دوره</h3>
-        <h3 className="w-[10%]">مدرس</h3>
-        <h3 className="w-[10%]">نام ترم</h3>
+        <h3 className="w-[12%]">نام دوره</h3>
+        <h3 className="w-[12%]">مدرس</h3>
+        <h3 className="w-[8%]">نام ترم</h3>
         <h3 className="w-[10%]">تاریخ شروع</h3>
-        <h3 className="w-[10%]">قیمت</h3>
+        <h3 className="w-[12%]">قیمت</h3>
       </div>
       <div className="flex flex-col gap-3">
         {courses.map((item, index) => (
@@ -61,19 +61,19 @@ const Table = () => {
             <div className="w-[10%] flex justify-center">
               <Image src={item.image} width={50} height={50} />
             </div>
-            <span className="w-[10%] overflow-hidden text-ellipsis whitespace-nowrap">
+            <span className="w-[12%] overflow-hidden text-ellipsis whitespace-nowrap">
               {item.name}
             </span>
-            <span className="w-[10%] overflow-hidden text-ellipsis whitespace-nowrap">
+            <span className="w-[12%] overflow-hidden text-ellipsis whitespace-nowrap">
               {item.teacher}
             </span>
-            <span className="w-[10%] overflow-hidden text-ellipsis whitespace-nowrap">
+            <span className="w-[8%] overflow-hidden text-ellipsis whitespace-nowrap">
               {item.termName}
             </span>
             <span className="w-[10%] overflow-hidden text-ellipsis whitespace-nowrap">
               {item.date}
             </span>
-            <span className="w-[10%] overflow-hidden text-ellipsis whitespace-nowrap">
+            <span className="w-[12%] overflow-hidden text-ellipsis whitespace-nowrap">
               <PriceFormat price={item.price}></PriceFormat>
             </span>
           </div>
@@ -83,4 +83,4 @@ const Table = () => {
   );
 };
 
-export default Table;
+export default DashboardTable;
