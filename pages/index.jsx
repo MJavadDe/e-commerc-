96158cc96bd46d90b60f68eb706024b8247cbdd9
Hoom90 <mohammad.mahdavi0015@gmail.com>
2023-04-13 11:21:21 +0330

    finalize-article&paymentLists

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,16 @@
 import Layout from "@/Components/layout";
-
+import Navbar from "@/Components/navbar/Navbar";
+import Searchbox from "@/Components/searchbox/searchbox";
 import React from "react";
+import CardList from "@/Components/payDetail cardList/CardPaymentList";
+import ArticleCard from "@/Components/articleCard/ArticleCard";
 
 const Home = () => {
-  return <Layout title="Home page"></Layout>;
+  return (
+    <Layout title="Home page">
+      {/* <Searchbox placeholder={"جستجوی دوره های مختلف"}></Searchbox> */}
+    </Layout>
+  );
 };
 
 export default Home;
