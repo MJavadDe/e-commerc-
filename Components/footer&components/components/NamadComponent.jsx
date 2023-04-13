0459f301ac4414bb5809bc1f1779cd9e407b99f7
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-13 15:09:03 +0430

    course Icons placed

--- Components/footer&components/components/NamadComponent.jsx
+++ Components/footer&components/components/NamadComponent.jsx
@@ -6,9 +6,6 @@ const NamadComponent = (props) => {
         <div className=' basis-6/12 w-1/3'>
             <SpecialHead className={`top-0 text-[#61CE70] text-[22px] py-1 `} title={props.content.title}/>
             <div className={`flex gap-7 pt-8 style justify-around w-full relative ${style.gradientbordertop}`}>
-                <div><img src={props.content.src1} className='bg-secondary-100 rounded-md p-1' alt="" /></div>
-                <div><img src={props.content.src2} className='bg-secondary-100 rounded-md p-1' alt="" /></div>
-                <div><img src={props.content.src3} className='bg-secondary-100 rounded-md p-1' alt="" /></div>
             </div>
         </div>
     );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,22 @@
+<<<<<<< Updated upstream
 import Layout from "@/Components/layout";
 import React from "react";
+=======
+import Layout from '@/Components/layout';
+import Navbar from '@/Components/navbar/Navbar';
+import Searchbox from '@/Components/searchbox/searchbox';
+import React from 'react';
+import CourseIcons from '../Components/courseIcons/CourseIcons';
+>>>>>>> Stashed changes
 
 const Home = () => {
   return (
     <Layout title="Home page">
+<<<<<<< Updated upstream
       {/* <Searchbox placeholder={"جستجوی دوره های مختلف"}></Searchbox> */}
+=======
+          <Searchbox placeholder={"جستجوی دوره های مختلف"}></Searchbox>
+>>>>>>> Stashed changes
     </Layout>
   );
 };
