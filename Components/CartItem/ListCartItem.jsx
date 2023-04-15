b4bc6298e8c55f81690df7e8598a204560845d04
binam <afsare.binam@gmail.com>
2023-04-15 19:51:18 +0430

    cart landing - total price in payment cart

--- Components/CartItem/ListCartItem.jsx
+++ Components/CartItem/ListCartItem.jsx
@@ -1,26 +1,11 @@
 import React from "react";
 import CartItem from "./CartItem";
 
-function ListCartItem(props) {
-  const listItem = [
-    {
-      id: 1,
-      title: "دوره ریکت جی اس پیش رفته صفر تا صد",
-      instructor: "استاد احسان مولایی",
-      price: "250000",
-      img: "/images/html.jpg",
-    },
-    {
-      id: 2,
-      title: "دوره ریکت جی اس پیش رفته صفر تا صد",
-      instructor: "استاد احسان مولایی",
-      price: "250000",
-      img: "/images/react.jpg",
-    },
-  ];
+function ListCartItem({className,data}) {
+ 
   return (
-    <div className="flex-col justify-between mx-auto bg-primary-light rounded flex gap-3 p-3 font-vazir">
-      {listItem.map((item, index) => (
+    <div className={`flex-col mx-auto bg-primary-light rounded-lg flex gap-3 p-3 font-vazir ${className}`}>
+      {data.map((item, index) => (
         <CartItem key={`item-index`} data={item} />
       ))}
     </div>
--- Components/payDetail cardList/CardPaymentList.jsx
+++ Components/payDetail cardList/CardPaymentList.jsx
@@ -6,35 +6,21 @@ import style from "./CardPaymentList.module.css";
 import PaymentCard from "../paymentCardList/PaymentCard";
 import PriceFormat from "@/utilitie/priceFormat";
 
-export default function CardPaymentList() {
+export default function CardPaymentList({ data }) {
   // const data = prop.paymentCardList
-  const data = [
-    {
-      name: "دوره نکست جی اس مقدماتی از صفر تا صد",
-      price: "80000",
-    },
-    {
-      name: "دوره نکست جی اس مقدماتی از صفر تا صد",
-      price: "90000",
-    },
-    {
-      name: "دوره نکست جی اس مقدماتی از صفر تا صد",
-      price: "90000",
-    },
-    {
-      name: "دوره نکست جی اس مقدماتی از صفر تا صد",
-      price: "90000",
-    },
-  ];
+  let totalPrice = 0;
+  data.map((item) => {
+    totalPrice += parseInt(item.price);
+  });
   return (
-    <div className=" rounded-md grid gap-3 p-[21px] border font-vazir">
+    <div className=" rounded-md grid gap-3 p-[21px] border font-vazir bg-white">
       <div
         className={`font-semibold border-primary-default pb-[20px] ${style.borderbuttomdashedcustom}`}
       >
         جزئیات پرداخت
       </div>
       <div className="grid gap-[8px] my-[10px]">
-        {data.map((item , index) => {
+        {data.map((item, index) => {
           return (
             <Fragment key={index}>
               <PaymentCard item={item} />
@@ -48,11 +34,13 @@ export default function CardPaymentList() {
         <div className="truncate col-span-4 font-semibold">
           مبلغ قابل پرداخت
         </div>
-        <div className="col-span-1 font-semibold whitespace-nowrap"><PriceFormat price={300000}/></div>
+        <div className="col-span-1 font-semibold whitespace-nowrap">
+          <PriceFormat price={totalPrice} />
+        </div>
       </div>
       <Button
         className={
-          "bg-secondary-light h-[50px] grid items-center text-white rounded-md hover:bg-primary-default cursor-pointer"
+          "bg-primary-dark h-[50px] grid items-center text-white rounded-md hover:bg-primary-default cursor-pointer"
         }
       >
         تکمیل فرایند خرید
