327c1e18d23749287f1168c034077aa7d95fb9c4
binam <afsare.binam@gmail.com>
2023-04-15 16:16:58 +0430

    merge confilict footer

--- Components/footer&components/Footer.jsx
+++ Components/footer&components/Footer.jsx
@@ -4,11 +4,7 @@ import FooterTop from './components/FooterTop';
 
 const Footer = () => {
     return (
-<<<<<<< Updated upstream
-        <div className='bg-[#3C6E71] w-full  mx-auto h-fit flex-col rounded-md font-vazir'>
-=======
         <div className='font-vazir bg-[#3C6E71] w-full mx-auto h-fit flex-col rounded-md py-5'>
->>>>>>> Stashed changes
             <FooterTop/>
             <FooterBottom/>
         </div>
