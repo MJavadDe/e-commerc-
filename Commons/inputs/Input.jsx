a1f104b081fde008fab36d775f3f846719ca86ce
binam <afsare.binam@gmail.com>
2023-04-15 18:19:23 +0430

    contact us

--- Commons/inputs/Input.jsx
+++ Commons/inputs/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({ type, placeholder, className }) => {
   return (
      <input
         dir="rtl"
-        className={`font-vazir rounded bg-white placeholder:text-primary shadow-md px-2 py-1 outline-none text-green-500 h-[50px] ${className}`}
+        className={`font-vazir rounded bg-white placeholder:text-primary shadow-md px-2 py-1 outline-none text-blue h-[50px] ${className}`}
         type={type}
         placeholder={placeholder}
       />
