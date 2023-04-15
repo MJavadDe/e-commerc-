0671859a9245edd14af5bccfa0cbfa2dfa2df660
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-15 17:14:57 +0430

    footer updated

--- Components/footer&components/Footer.jsx
+++ Components/footer&components/Footer.jsx
@@ -6,6 +6,9 @@ const Footer = () => {
     return (
 <<<<<<< Updated upstream
         <div className='bg-[#3C6E71] w-full  mx-auto h-fit flex-col rounded-md font-vazir'>
+=======
+        <div className='font-vazir bg-[#3C6E71] w-full mx-auto h-fit flex-col rounded-md py-5'>
+>>>>>>> Stashed changes
             <FooterTop/>
             <FooterBottom/>
         </div>
