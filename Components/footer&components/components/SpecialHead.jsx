b25bfb2e4213a80a9d30934b90c54c7368ca6f5d
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-04-16 17:30:11 +0430

    specialHead updated

--- Components/footer&components/components/SpecialHead.jsx
+++ Components/footer&components/components/SpecialHead.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import style from './Footer.module.css'
 
 const SpecialHead = ({className}) => {
+const SpecialHead = ({className, title}) => {
     return (
         <p className={`${className} ${style.gradientborderbottom} `}>
-           {props.title} 
         </p>
     );
 };
--- Components/teacherCarousel/Teachers.jsx
+++ Components/teacherCarousel/Teachers.jsx
@@ -30,9 +30,11 @@ const Teachers = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        // centerPadding: "100px",
         nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
+        prevArrow: <SamplePrevArrow />,
     }
     
     return (
