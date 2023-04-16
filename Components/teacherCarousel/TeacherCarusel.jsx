e211c2f05cbeb16065259f4eb02b3bf03b7775d2
binam <afsare.binam@gmail.com>
2023-04-16 17:40:29 +0430

    teacher carusel responsive

--- Components/teacherCarousel/TeacherCarusel.jsx
+++ Components/teacherCarousel/TeacherCarusel.jsx
@@ -4,8 +4,8 @@ import Teachers from './Teachers';
 
 function TeacherCarusel(props) {
     return (
-        <div className='w-full bg-secondary-very-light min-h-[300px] rounded-lg flex flex-col items-center justify-between lg:flex-row-reverse px-15 pb-7 pt-9 font-vazir gap-8'>
-            <div className="min-h-[300px] lg:min-h-[400px] lg:w-1/2 bg-[url('/images/teacher.png')] bg-contain bg-center bg-no-repeat">
+        <div className='w-full bg-secondary-very-light min-h-[300px] rounded-lg flex flex-col items-center justify-between lg:flex-row-reverse px-5 md:px-15 pb-7 pt-9 font-vazir gap-8'>
+            <div className="w-full min-h-[300px] lg:w-1/2 bg-[url('/images/teacher.svg')] bg-contain bg-center bg-no-repeat">
 
             </div>
             <div className='w-full lg:w-1/2 flex-col justify-center items-center text-center'>
