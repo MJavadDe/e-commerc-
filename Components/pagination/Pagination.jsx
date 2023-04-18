ff8750c705c4c577017ddf85e1de76fb29a32e6e
Hadis Yazdi <69729120+hadis-yzd@users.noreply.github.com>
2023-04-18 17:41:41 +0430

    pagination tada

--- Components/pagination/Pagination.jsx
+++ Components/pagination/Pagination.jsx
@@ -1,11 +1,9 @@
-
-
 import { Pagination } from "antd";
 import { useState } from "react";
 import React from "react";
 import styles from "./pagaination.module.css";
 
-function Paginations({total , mypage}) {
+function Paginations({ total, mypage }) {
   console.log(mypage);
   const [current, setCurrent] = useState(mypage);
   const onChange = (page) => {
@@ -13,18 +11,17 @@ function Paginations({total , mypage}) {
     setCurrent(page);
   };
   return (
-    <div className="flex justify-center h-16 items-center w-[320px] shadow-all rounded-lg">
+    <div className="flex justify-center h-16 mx-auto items-center w-[320px] shadow-all rounded-lg">
       <Pagination
-      className={styles.anticon}
-      current={current}
-      onChange={onChange}
-      total={50}
-    />
+        className={styles.anticon}
+        current={current}
+        onChange={onChange}
+        total={50}
+      />
     </div>
   );
 }
 
 export default Paginations;
-
 
 // class="ant-pagination-item ant-pagination-item-5 ant-pagination-item-active"
