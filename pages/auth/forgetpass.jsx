ce388541b779870b8ff36b32940a6ac21de8a96f
amin <94073967+aminyaghmaee@users.noreply.github.com>
2023-04-18 15:14:55 +0430

    forgetpass issue fixed

--- pages/auth/forgetpass.jsx
+++ pages/auth/forgetpass.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const ForgetPass = ({protect}) => {
   return (
-    <div className="flex justify-center bg-circle lg:justify-between bg-primary-very-light py-20 lg:px-10">
+    <div className="flex justify-center bg-circle lg:justify-between bg-primary-very-light py-[15rem] lg:px-10">
       <div className="flex flex-col items-center w-[90%] lg:w-1/3 lg:mr-20 gap-10 z-10 p-5">
         <h3 className="text-blue">فراموشی رمز عبور</h3>
         <Input className="w-full rounded-xl" placeholder="ایمیل" />
